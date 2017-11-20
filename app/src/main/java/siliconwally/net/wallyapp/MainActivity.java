19185b08e5f8a2967ef87c18c743d85ac9c12737
Cristian Mamani <cristian.mamani@mojix.com>
2017-11-20 12:12:47 -0400

    Apk has been added

--- app/src/main/java/siliconwally/net/wallyapp/MainActivity.java
+++ app/src/main/java/siliconwally/net/wallyapp/MainActivity.java
@@ -1,10 +1,12 @@
 package siliconwally.net.wallyapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Chronometer;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,6 +29,16 @@ public class MainActivity extends AppCompatActivity {
         button.setText(String.valueOf(intValue));
         System.out.println("Valor:  " + value);
 
+
+        Intent whatsappIntent = new Intent(Intent.ACTION_SEND);
+        whatsappIntent.setType("text/plain");
+        whatsappIntent.setPackage("com.whatsapp");
+        whatsappIntent.putExtra(Intent.EXTRA_TEXT, "The text you wanted to share");
+        try {
+            startActivity(whatsappIntent);
+        } catch (android.content.ActivityNotFoundException ex) {
+            Toast.makeText(getApplicationContext(),"Whatsapp have not been installed.", Toast.LENGTH_LONG).show();
+        }
 
     }
 }
