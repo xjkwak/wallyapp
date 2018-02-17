7248e2624a0ff6c6762552b784b2845dc2819b0a
Jhamil <jhamil.conaca@mojix.com>
2018-02-17 18:39:42 -0400

    Add textView dynamically

--- app/src/main/java/siliconwally/net/wallyapp/ScoreboardActivity.java
+++ app/src/main/java/siliconwally/net/wallyapp/ScoreboardActivity.java
@@ -1,10 +1,15 @@
 package siliconwally.net.wallyapp;
 
+import android.app.ActionBar;
+import android.graphics.Color;
 import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.google.firebase.database.DataSnapshot;
@@ -12,6 +17,8 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class ScoreboardActivity extends BaseActivity {
 
@@ -48,10 +55,12 @@ public class ScoreboardActivity extends BaseActivity {
         nameB = findViewById(R.id.nameB);
         setScoreA = findViewById(R.id.setScoreA);
         setScoreB = findViewById(R.id.setScoreB);
-        detailedScoreA = findViewById(R.id.detailedScoreA);
-        detailedScoreB = findViewById(R.id.detailedScoreB);
-        detailedScoreASets = findViewById(R.id.detailedScoreASets);
-        detailedScoreBSets = findViewById(R.id.detailedScoreBSets);
+
+        //detailedScoreA = findViewById(R.id.detailedScoreA);
+        //detailedScoreB = findViewById(R.id.detailedScoreB);
+
+        //detailedScoreASets = findViewById(R.id.detailedScoreASets);
+        //detailedScoreBSets = findViewById(R.id.detailedScoreBSets);
 
 //        updateScoreboard();
 
@@ -104,9 +113,45 @@ public class ScoreboardActivity extends BaseActivity {
         nameB.setText(match.getTeamB());
         setScoreA.setText(String.valueOf(match.getCountA()));
         setScoreB.setText(String.valueOf(match.getCountB()));
-        detailedScoreA.setText(match.getTeamA());
-        detailedScoreB.setText(match.getTeamB());
-        detailedScoreASets.setText(match.getPointsA().toString());
-        detailedScoreBSets.setText(match.getPointsB().toString());
+
+        //detailedScoreA.setText(match.getTeamA());
+        //detailedScoreB.setText(match.getTeamB());
+
+        //detailedScoreASets.setText(match.getPointsA().toString());
+
+        LinearLayout llDetailA = (LinearLayout) findViewById(R.id.llDetailA);
+        fillDetailScore(llDetailA, match.getPointsA(), match.getTeamA());
+
+        //detailedScoreBSets.setText(match.getPointsB().toString());
+        LinearLayout llDetailB = (LinearLayout) findViewById(R.id.llDetailB);
+        fillDetailScore(llDetailB, match.getPointsB(), match.getTeamB());
+    }
+
+    private void fillDetailScore(LinearLayout linear, ArrayList<Integer> list, String team) {
+        TextView nameTeam = new TextView(this);
+        nameTeam.setText(team);
+        LinearLayout.LayoutParams llp = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.TEXT_ALIGNMENT_GRAVITY);
+        llp.setMargins(0,0,20,0);
+        llp.weight = 1.0f;
+        nameTeam.setLayoutParams(llp);
+        nameTeam.setTextSize(20);
+        nameTeam.setGravity(Gravity.LEFT);
+        nameTeam.setPadding(0,0,20,0);
+        linear.addView(nameTeam);
+        for (int item: list) {
+            TextView text = new TextView(this);
+            text.setText(String.valueOf(item));
+            /*LinearLayout.LayoutParams llp = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT,
+                    ViewGroup.TEXT_ALIGNMENT_GRAVITY);
+            llp.setMargins(0,0,20,0);
+            llp.weight = 1;*/
+            text.setLayoutParams(llp);
+            text.setTextSize(20);
+            text.setGravity(Gravity.LEFT);
+            text.setBackgroundColor(Color.CYAN);
+            text.setPadding(20,0,10,0);
+            linear.addView(text);
+        }
     }
 }
